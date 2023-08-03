1d176887d97d230cd01f118efb1fb5ac348c0207
Vctr-Ost <ostapenko.vctr@gmail.com>
2023-08-04 00:30:24 +0300

    Added 1 category and 2 new courses.

--- shop/models.py
+++ shop/models.py
@@ -3,12 +3,12 @@ from django.utils import timezone
 
 class Category(models.Model):
     title = models.CharField(max_length=255)
-    created_at = models.DateTimeField(timezone.now)
+    created_at = models.DateTimeField(default=timezone.now)
 
 class Course(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=128)
     price = models.FloatField()
     students_qty = models.IntegerField()
     reviews_qty = models.IntegerField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(timezone.now)
+    created_at = models.DateTimeField(default=timezone.now)
