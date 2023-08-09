f0cf2695a82825cc5742d14053b19658407c8bce
Vctr-Ost <ostapenko.vctr@gmail.com>
2023-08-09 23:15:48 +0300

    Stage of DB integration

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import models
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello from the shop app!')
+    courses = models.Course.objects.all()
+    res = ''
+    for c in courses:
+        res += f"<br>{str(c)}"
+
+    return HttpResponse(res)
