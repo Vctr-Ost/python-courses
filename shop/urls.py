9988a63f25e5d07aa0794b3563122c83ac93c722
Vctr-Ost <ostapenko.vctr@gmail.com>
2023-08-14 11:08:11 +0300

    Full routing

--- shop/urls.py
+++ shop/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import index, single_course
 
 
+app_name = 'shop'
 
 urlpatterns = [
     path('', index, name='index'),
--- shop/views.py
+++ shop/views.py
@@ -1,14 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from . import models
 
 # Create your views here.
 def index(request):
     courses = models.Course.objects.all()
     # return HttpResponse(str(course)+'<br>' for course in courses)
-    return render(request, 'courses.html', {'courses': courses})
+    return render(request, 'shop/courses.html', {'courses': courses})
 
 
 def single_course(request, course_id):
-    course = models.Course.objects.get(pk=course_id)
-    return render(request, 'single_course.html', {'course': course})
+    # try:
+    #     course = models.Course.objects.get(pk=course_id)
+    #     return render(request, 'single_course.html', {'course': course})
+    # except models.Course.DoesNotExist:
+    #     raise Http404
+
+    course = get_object_or_404(models.Course, pk=course_id)
+    return render(request, 'shop/single_course.html', {'course': course})
